82639643b43f64ba1d28e71cfacf4dd868f30136
Adriano M. da Rosa <71461040+adriano-rosa@users.noreply.github.com>
2022-09-29 23:24:10 -0300

    41_adicionando_css_dom

---
+++ 41_adicionando_css_dom/js/scripts.js
@@ -0,0 +1,20 @@
+// como adicionar css ao título principal
+
+// selecionar o elemento - #title
+var title = document.querySelector("#title");
+
+// adicionar o css para uma propriedade desse objeto (adiciona o estilo)
+title.style.color = "red";
+
+// background-color
+title.style.backgroundColor = "yellow";
+
+
+// selecionar o elemento
+var subtitle = document.querySelector(".subtitle");
+
+// adicionar vários estilos
+subtitle.style.cssText = "color: blue; background-color: pink; font-size: 50px";
+
+
+
