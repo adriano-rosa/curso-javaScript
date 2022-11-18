771f4e9788d973e4b7011f7e6c5cb070c4a84be5
Adriano M. da Rosa <71461040+adriano-rosa@users.noreply.github.com>
2022-11-18 17:00:21 -0300

    50_finalizacao_projeto

--- 50_projeto/js/scripts.js
+++ 50_projeto/js/scripts.js
@@ -1,1 +1,61 @@
-console.log("testando");
+
+// função que adciona tarefa
+function addTask() {
+    // título da tarefa
+    const taskTitle = document.querySelector("#task-title").value;
+
+    if(taskTitle) { // se existe taskTitle
+        // colna o template
+        const template = document.querySelector(".template");
+
+        const newTask = template.cloneNode(true); // clonenode clona o html em uma nova variável
+
+        // adiciona título
+        newTask.querySelector(".task-title").textContent = taskTitle;
+
+        // remove as classes desnecessárias
+        newTask.classList.remove("template");
+        newTask.classList.remove("hide");
+
+        // adiciona tarefa na lista
+        const list = document.querySelector("#task-list");
+
+        list.appendChild(newTask);
+
+        // adicionar o evento remover
+        const removeBtn = newTask.querySelector(".remove-btn").addEventListener("click", function() {
+            removeTask(this);
+        });
+
+        // adicionar evento de completar tarefa
+        const doneBtn = newTask.querySelector(".done-btn").addEventListener("click", function() {
+            completeTask(this);
+        });
+
+        // limpar texto
+        document.querySelector("#task-title").value = "";
+
+    }
+}
+
+// função de remover tarefa
+function removeTask(task) {
+    task.parentNode.remove(true);
+}
+
+// função de completar tarefa
+function completeTask(task) {
+    const taskToComplete = task.parentNode;
+
+    taskToComplete.classList.toggle("done");
+}
+
+// Evento de adicionar tarefa
+const addBtn = document.querySelector("#add-btn");
+
+addBtn.addEventListener("click", function(e) {
+    e.preventDefault();
+
+    addTask();
+
+});
