7c7a047f59053478c6e352c7d7f894f8e3bf4ad2
Adriano M. da Rosa <71461040+adriano-rosa@users.noreply.github.com>
2022-11-18 11:15:40 -0300

    Aula_47_evento_click

---
+++ 47_evento_click/js/scripts.js
@@ -0,0 +1,34 @@
+/* O click é ativado quando o usuário clica em um elemento 
+em que atrelamos o evento */
+
+var btn = document.querySelector("#btn");
+
+console.log(btn);
+
+btn.addEventListener("click", function() {
+    console.log("clicou");
+
+    console.log(this);
+
+    this.style.color = "red";
+
+});
+
+// click afetando outros elementos
+var title = document.querySelector("#title");
+
+title.addEventListener("click", function() {
+    
+    var subtitle = document.querySelector(".subtitle");
+
+    subtitle.style.display = "none";
+});
+
+// double click
+var subtitle = document.querySelector(".subtitle");
+
+subtitle.addEventListener("dblclick", function() {
+
+    console.log("click duplo");
+
+});
