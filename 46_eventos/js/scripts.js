85a5173fd55550dee806a29f675c3bc30054d522
Adriano M. da Rosa <71461040+adriano-rosa@users.noreply.github.com>
2022-11-18 11:14:59 -0300

    Aula_46_Eventos

---
+++ 46_eventos/js/scripts.js
@@ -0,0 +1,16 @@
+/* O Onload é ativado ao carregar a página
+Depois desse evento podemos realizar alguma ação no nosso projeto*/
+
+window.onload = function() {
+    console.log("Carregou o DOM");
+
+    var title2 = document.querySelector("#title");
+
+    console.log(title2);
+}
+
+console.log("Carregou o JS");
+
+var title = document.querySelector("#title");
+
+console.log(title);
